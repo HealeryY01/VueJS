b29217b71e6eb4add61d8430cff98f06db654b40
Healer <nguyenthehieua2001@gmail.com>
2024-03-28 18:44:41 +0700

    first code

--- Chapter2/app-vue-component/src/App.vue
+++ Chapter2/app-vue-component/src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <h1> Hello World !</h1>
+  <FirstComponent></FirstComponent>
+  <SecondComponent></SecondComponent>
+  <ProductComponent/>
 </template>
 
 <script>
+import FirstComponent from './components/FirstComponent.vue';
+import SecondComponent from './components/SecondComponent.vue';
+import ProductComponent from './components/ProductComponent.vue'
 export default {
   name: 'App',
   components: {
-  }
-}
+    FirstComponent,
+ SecondComponent,
+ ProductComponent,
+
+  },
+};
 </script>
 
 <style>
---
+++ Chapter2/app-vue-component/src/components/ProductComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <section class="container">
+        <div class="row">
+
+            <div class="col-sm-3">
+                <div class="card text-left">
+                    <img class="card-img-top"
+                        src="https://salt.tikicdn.com/cache/280x280/ts/product/0b/1a/98/b36c67698cc86e194d17ef9e0c6783a5.jpg.webp"
+                        alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">MacBook Air M1 13 inch 2020</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card text-left">
+                    <img class="card-img-top"
+                        src="https://salt.tikicdn.com/cache/280x280/ts/product/0b/1a/98/b36c67698cc86e194d17ef9e0c6783a5.jpg.webp"
+                        alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">MacBook Air M1 13 inch 2020</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card text-left">
+                    <img class="card-img-top"
+                        src="https://salt.tikicdn.com/cache/280x280/ts/product/0b/1a/98/b36c67698cc86e194d17ef9e0c6783a5.jpg.webp"
+                        alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">MacBook Air M1 13 inch 2020</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card text-left">
+                    <img class="card-img-top"
+                        src="https://salt.tikicdn.com/cache/280x280/ts/product/0b/1a/98/b36c67698cc86e194d17ef9e0c6783a5.jpg.webp"
+                        alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">MacBook Air M1 13 inch 2020</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
